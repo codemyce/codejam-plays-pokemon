eb33369434109a940fda1c741d4282e9b53ad922
minhradz <minhradz@gmail.com>
2019-11-16 14:22:58 -0500

    code rewrite everything, added the keys mappings

--- KeyboardInput.py
+++ KeyboardInput.py
@@ -1,6 +1,53 @@
 import keyboard
 from pywinauto import Application
+import time
+import string
+from threading import *
 
+# i use the bsnes emulator from here
 # open up the emulator and load up the rom
+
 emulator = Application().start("bsnes-hd_beta9.exe")
 keyboard.press_and_release('alt, enter, down, enter, enter')
+time.sleep(5)
+
+# listenning keyboard activitess
+keys = list(string.ascii_lowercase)
+
+def listen(key):
+    while True:
+        keyboard.wait(key)
+        print("[+] Pressed", key)
+
+threads = [Thread(target=listen, kwargs={"key": key}) for key in keys]
+for thread in threads:
+       thread.start()
+
+"""
+    player 1 controls:  up = up
+                        down = down
+                        left = left
+                        right = right
+                        
+                        A = y
+                        B = u
+                        X = h 
+                        Y = j
+                        
+    player 2 controls:  up = w
+                        down = s
+                        left = a
+                        right = d
+                        
+                        A = i
+                        B = o
+                        X = k
+                        Y = l
+                        
+    Will have to load the config to bsnes first !
+"""
+
+
+
+
+
