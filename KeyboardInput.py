21fcdbd7fcf2303af924cdcb34eefaf1a5eeb6bf
JD908 <46234488+JD908@users.noreply.github.com>
2019-11-16 18:16:01 -0500

    keyboard shit

--- KeyboardInput.py
+++ KeyboardInput.py
@@ -2,8 +2,6 @@ import keyboard
 from pywinauto import Application
 import time
 import string
-import msvcrt
-import readchar
 # open up the emulator and load up the rom
 
 emulator = Application().start("bsnes-hd_beta9.exe")
@@ -11,19 +9,31 @@ print('hello')
 keyboard.press_and_release('alt, enter, down, enter, enter')
 
 while True:
+    time.sleep(0.08)
+    # try:
+    #     if keyboard.is_pressed('left'):
+    #         keyboard.send('left',)
+    #     elif keyboard.is_pressed('right'):
+    #         print('right key is pressed')
+    #         keyboard.send('right')
+    #     else:
+    #         pass
+    #     if keyboard.is_pressed('u'):
+    #         print('B button is pressed')
+    #         keyboard.release('y')
+    #         keyboard.send('u')
+    #     elif keyboard.is_pressed('y'):
+    #         print('A button is pressed')
+    #         keyboard.send('y',do_release=False)
+    #     else:
+    #         pass        
+    # except:
+    #     pass
     try:
-        if keyboard.is_pressed('left'):
-            print('left key is pressed')
-        elif keyboard.is_pressed('right'):
-            print('right key is pressed')
-        elif keyboard.is_pressed('u'):
-            print('B button is pressed')
-        elif keyboard.is_pressed('y'):
-            print('A button is pressed')
-        else:
-            pass
+        if keyboard.is_pressed('x'):
+            keyboard.send('u',do_release=False)
     except:
-        break
+        pass
 
 """
 threads = [Thread(target=listen, kwargs={"key": key}) for key in keys]
